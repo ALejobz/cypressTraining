ae81d1fcf6fa8e963d8988a4479ebcc7d89cadc4
Alejandro Beltran Zuluaga <Alejandro.Beltran@endava.com>
2023-06-16 13:27:36 -0500

    Homework 4

--- cypress/e2e/spec4.cy.js
+++ cypress/e2e/spec4.cy.js
@@ -5,14 +5,24 @@ describe('template spec', () => {
   const tipe = 'electric'
 
   it('passes', () => {
+    cy.request('GET','https://pokeapi.co/api/v2/pokemon/pikachu')
+    .its('status').should('equal',200)
+  })
+
+  it('passes', () => {
     cy.request({method:"GET", url:"https://pokeapi.co/api/v2/pokemon/beltran",failOnStatusCode:false })
     .its('status').should('equal', 404)
   })
 
   it('passes', () => {
     cy.request({method:"GET", url:"https://pokeapi.co/api/v2/pokemon/pikachu",failOnStatusCode:false })
-    .its('body.name').should('equal', 'pikachu')
+    .its('body.name').should('equal', poke)
   })
+  
+  it('passes', () => {
+    cy.request('GET', 'https://pokeapi.co/api/v2/pokemon/'+poke).its('body.types[0].type.name').should('equal',tipe)  
+  })
+
   it('passes', () => {
     cy.request({method:"GET", url:"https://pokeapi.co/api/v2/pokemon/"+poke,failOnStatusCode:false }).then(
       (response)=>{
